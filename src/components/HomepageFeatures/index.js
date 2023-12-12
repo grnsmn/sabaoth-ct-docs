e41cdd7056c1af989f841d3709075fa7869c2f35
Obsidian <simoneguarnuccio@gmail.com>
2023-12-12 22:40:29 +0100

    feat: used new asset in Home

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Vuoi conoscere quali strumenti digitali vengono usati o vorresti iniziare a collaborarci? ',
-    Svg: require('@site/static/img/logoBK.svg').default,
+    Svg: require('@site/static/img/logoBlack.svg').default,
     description: (
       <>
        Questo sito è pensato proprio per la formazione o come punto di riferimento per la conoscenza degli strumenti e 
@@ -14,26 +14,6 @@ const FeatureList = [
       </>
     ),
   },
-  // {
-  //   title: 'Focus on What Matters',
-  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-  //   description: (
-  //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-  //       ahead and move your docs into the <code>docs</code> directory.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
 ];
 
 function Feature({Svg, title, description}) {
