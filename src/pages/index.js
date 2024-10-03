e31a3f02e2db12067cd24c1bdb23f98cc2f1af2e
Obsidian <simoneguarnuccio@gmail.com>
2024-10-03 23:22:16 +0200

    feat: try to integrate donorbox button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,50 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Vai alla Docs  
+            to="/docs/intro"
+          >
+            Vai alla Docs
           </Link>
         </div>
         <div className={styles.buttons}>
-        <Link
+          <Link
             className="button button--secondary button--lg"
-            to="/docs/Team%20Media/Calendario%20Turni">
-            Vai al Calendari dei turni  
+            to="/docs/Team%20Media/Calendario%20Turni"
+          >
+            Vai al Calendari dei turni
           </Link>
         </div>
+        <a class="custom-dbox-popup" href="https://donorbox.org/aftergospel">
+          <img src="https://donorbox.org/images/png-donate/button-medium-blue.png" />
+        </a>
       </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
