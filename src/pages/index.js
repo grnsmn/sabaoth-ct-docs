a3f26471b5fc4a2b8556ad4cff68ab729d5e84e6
Obsidian <simoneguarnuccio@gmail.com>
2025-02-04 22:33:50 +0100

    feat: Add Eventi documentation page and update homepage links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,56 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className='container'>
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/Calendario%20Turni">
-            Vai al Calendari dei turni  
+            className='button button--secondary button--lg'
+            to='/docs/Calendario%20Turni'
+          >
+            Vai al Calendari dei turni
           </Link>
         </div>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
+            className='button button--secondary button--lg'
+            to='/docs/intro'
+          >
             Perchè questa documentazione?
           </Link>
         </div>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/Team Media/Intro">
+            className='button button--secondary button--lg'
+            to='/docs/Team Media/Intro'
+          >
             Documentazione Team Media
           </Link>
         </div>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/DatiDonazione">
+            className='button button--secondary button--lg'
+            to='/docs/Eventi'
+          >
+            Eventi in Programma
+          </Link>
+        </div>
+        <div className={styles.buttons}>
+          <Link
+            className='button button--secondary button--lg'
+            to='/docs/DatiDonazione'
+          >
             Vai alla pagina di Donazione
           </Link>
         </div>
@@ -48,11 +60,12 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description='Description will go into a meta tag in <head />'
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
