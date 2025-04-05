5048a4c3e1ee041833bb519e7fb2ef5d811ca85a
Obsidian <simoneguarnuccio@gmail.com>
2025-04-05 17:50:28 +0200

    fix: Enable respect for prefers-color-scheme in Docusaurus configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: false,
-        respectPrefersColorScheme: false,
+        respectPrefersColorScheme: true,
       },
       navbar: {
         title: "Documentazione",
